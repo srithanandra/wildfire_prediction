52a68821c8695940c7dffa657ac381144d327d6e
srithanandra <srithanandra@gmail.com>
2026-01-28 23:27:59 -0800

    completed reading datasets and converting to usable

--- src/download/download_fires.py
+++ src/download/download_fires.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import config
 
 url = 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/c6.1/csv/MODIS_C6_1_USA_contiguous_and_Hawaii_24h.csv'
 fires = pd.read_csv(url)
 fires = fires[['latitude', 'longitude', 'acq_date']]
-fires.to_csv(f'{config.RAW_DIR}\\fires.csv', index=False)
-print(f'Completed: Data saved to {config.RAW_DIR}\\fires.csv')
+fires.to_csv(f'..\\..\\data\\raw\\fires.csv', index=False)
+print(f'Completed: Data saved to fires.csv in data\\raw')
--- src/preprocess/build_features.py
+++ src/preprocess/build_features.py
@@ -1,19 +1,17 @@
 import pandas as pd
-import numpy as np
-from src.config import RAW_DIR, PROCESSED_DIR, RADIUS_KM
 from src.preprocess.spatial_join import spatial_match
+import config
 
-fires = pd.read_csv(f'{RAW_DIR}\\fires.csv')
+fires = pd.read_csv(f'..\\..\\data\\raw\\fires.csv')
 fires['date'] = pd.to_datetime(fires['acq_date']) + pd.Timedelta(days=1)
 
-weather = pd.read_csv(f'{RAW_DIR}\\weather.csv')
-weather['date'] = pd.to_datetime(weather['time'])
+weather = pd.read_csv(f'..\\..\\data\\raw\\weather.csv')
+weather['date'] = pd.to_datetime(weather['valid_time'])
 weather['temperature'] = weather['t2m'] - 273.15
-weather['wind_speed'] = (weather['u10']**2 + weather['v10']**2)**0.5
 
-mask = spatial_match(weather, fires, RADIUS_KM)
+mask = spatial_match(weather, fires, config.RADIUS_KM)
 weather['label'] = mask.astype(int)
 
-data = weather[['temperature', 'wind_speed', 'tp', 'label']]
+data = weather[['temperature', 'tp', 'label']]
 data = data.dropna()
-data.to_csv(f'{PROCESSED_DIR}\\dataset.csv', index=False)
+data.to_csv(f'..\\..\\data\\processed\\dataset.csv', index=False)
