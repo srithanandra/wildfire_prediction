284a7956cdc2747b591d5e160df5dc9f11a51225
srithanandra <srithanandra@gmail.com>
2026-01-28 23:10:06 -0800

    updated directories and organization

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-DATA_DIR = 'data'
-RAW_DIR = 'data/raw'
-PROCESSED_DIR = 'data/processed'
+DATA_DIR = 'C:\\Users\\andra\\Desktop\\Projects\\wildfire_prediction\\data'
+RAW_DIR = DATA_DIR + '\\raw'
+PROCESSED_DIR = DATA_DIR + '\\processed'
 RADIUS_KM = 5
--- src/download/download_ndvi.py
+++
@@ -1,7 +0,0 @@
-import xarray as xr
-
-RAW_DIR = 'data/raw'
-
-ds = xr.open_dataset('modis_ndvi.nc') # download this dataset
-df = ds.to_dataframe().reset_index()
-df.to_csv(f'{RAW_DIR}/ndvi.csv', index=False)
