92c5f417cc6e742f5c1c06f1a1f477cbb001c316
srithanandra <srithanandra@gmail.com>
2026-01-28 23:27:59 -0800

    completed reading datasets and converting to usable

--- src/config.py
+++ src/config.py
@@ -1,4 +1,1 @@
-DATA_DIR = 'C:\\Users\\andra\\Desktop\\Projects\\wildfire_prediction\\data'
-RAW_DIR = DATA_DIR + '\\raw'
-PROCESSED_DIR = DATA_DIR + '\\processed'
 RADIUS_KM = 5
--- src/download/download_weather.py
+++ src/download/download_weather.py
@@ -1,19 +1,15 @@
 import xarray as xr
 import config
 
-# Open each dataset
-tp_dataset = xr.open_dataset('C:\\Users\\andra\\Downloads\\weather_datasets\\total_precipitation_0_daily-mean.nc')
-t2m_dataset = xr.open_dataset('C:\\Users\\andra\\Downloads\\weather_datasets\\2m_temperature_0_daily-mean.nc')
-d2m_dataset = xr.open_dataset('C:\\Users\\andra\\Downloads\\weather_datasets\\2m_dewpoint_temperature_stream-oper_daily-mean.nc')
+tp_dataset = xr.open_dataset('..\\..\\data\\weather_datasets\\total_precipitation_0_daily-mean.nc')
+t2m_dataset = xr.open_dataset('..\\..\\data\\weather_datasets\\2m_temperature_0_daily-mean.nc')
+d2m_dataset = xr.open_dataset('..\\..\\data\\weather_datasets\\2m_dewpoint_temperature_stream-oper_daily-mean.nc')
 
-# Merge all datasets together (with compat='override' to avoid warning)
 dataset = xr.merge([tp_dataset, t2m_dataset, d2m_dataset], compat='override')
 
-# Check what variables are available
 print("Available variables:", list(dataset.data_vars))
 
-# Extract the variables you need
 dataframe = dataset[['t2m', 'tp']].to_dataframe().reset_index()
 dataframe.to_csv(f'{config.RAW_DIR}\\weather.csv', index=False)
 
-print(f"Data saved to {config.RAW_DIR}\\weather.csv")
+print(f"Completed: Data saved to {config.RAW_DIR}\\weather.csv")
