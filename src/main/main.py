970612ded6b91bbf27dddf42ea999d542f9958bf
srithanandra <srithanandra@gmail.com>
2026-02-07 20:22:06 -0800

    completed full functionality / need to find a better way to train model

--- src/main/main.py
+++ src/main/main.py
@@ -1,4 +1,7 @@
 import download.download_fires, download.download_weather, model.train, model.evaluate, preprocess.build_features
+from time import time
+
+start_time = time()
 
 download.download_fires.main()
 print('Downloaded fires')
@@ -10,3 +13,6 @@ model.train.main()
 print('Trained model')
 model.evaluate.main()
 print('COMPLETED PROCESS')
+
+end_time = time()
+print(f'TOTAL TIME: {(end_time-start_time)//60} minutes and {(end_time-start_time)%60} seconds')
